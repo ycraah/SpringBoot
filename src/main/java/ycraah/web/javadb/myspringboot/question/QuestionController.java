8d1971142ef844350705a6a99dfd9431fe4dfb75
ycraah <ycraah@gmail.com>
2024-08-27 22:15:04 +0900

    Controller 매핑 테스트

---
+++ src/main/java/ycraah/web/javadb/myspringboot/question/QuestionController.java
@@ -0,0 +1,15 @@
+package ycraah.web.javadb.myspringboot.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuestionController {
+
+  @GetMapping("/question/list")
+  @ResponseBody
+  public String list() {
+    return "question list";
+  }
+}
