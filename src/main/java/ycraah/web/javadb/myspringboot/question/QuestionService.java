be0b7a28e28a049ac43fb4393a26c7d00f5e75fa
ycraah <ycraah@gmail.com>
2024-08-29 00:51:14 +0900

    상세페이지 출력

--- src/main/java/ycraah/web/javadb/myspringboot/question/QuestionService.java
+++ src/main/java/ycraah/web/javadb/myspringboot/question/QuestionService.java
@@ -2,8 +2,10 @@ package ycraah.web.javadb.myspringboot.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ycraah.web.javadb.myspringboot.util.DataNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,5 +13,14 @@ public class QuestionService {
   private final QuestionRepository questionRepository;
   public List<Question> getList() {
     return this.questionRepository.findAll();
+  }
+
+  public Question getQuestion(Integer id){
+    Optional<Question> question = this.questionRepository.findById(id);
+    if(question.isPresent()){
+      return question.get();
+    } else {
+      throw new DataNotFoundException("question not found");
+    }
   }
 }
