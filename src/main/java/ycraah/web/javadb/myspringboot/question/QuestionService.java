effc32485ad8a55eb4395973871f86f87d7e095c
ycraah <ycraah@gmail.com>
2024-08-28 15:31:39 +0900

    컨트롤러에서 버ëì 서비스 사용하기

---
+++ src/main/java/ycraah/web/javadb/myspringboot/question/QuestionService.java
@@ -0,0 +1,15 @@
+package ycraah.web.javadb.myspringboot.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+  private final QuestionRepository questionRepository;
+  public List<Question> getList() {
+    return this.questionRepository.findAll();
+  }
+}
